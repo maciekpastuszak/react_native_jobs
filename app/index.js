d09dd91efc9b7ff4944dcbace8643cb7867000c9
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-26 08:50:30 +0200

    add imports to Home view

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
+    const router = useRouter()
     return (
-        <View>
+        <SafeAreaView>
             <Text>Home</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
