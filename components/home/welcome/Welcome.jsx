072a1d239942bb77d7bcb55ff415fc98cf6869a1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-26 09:40:40 +0200

    add welcome view in Welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -6,9 +6,13 @@ import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Maciej</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
     </View>
   )
 }
