1d05042bda00dc79aa04a0d1830e91ac4f20813e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-26 09:38:19 +0200

    add imports to Welcome

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
   return (
