aff1d6a386193f3f26d0787d1e9ea41cb861c8b1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-28 11:48:05 +0200

    add styles to Flatlist

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -26,13 +26,15 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <Flatlist 
-            data={[1,2,3,4]}
+            data={[1,2,3,4,5,6,7,8]}
             renderItem={({item}) => (
               <PopularJobCard 
                 item={item}
               />
             )}
             keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium}}
+            horizontal
           />
         )}
       </View>
