eb204801d002d9f57f5aca566f9900438effb6bf
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-31 23:18:04 +0200

    add position names in popular jobs

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -16,6 +16,11 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
       />
     </TouchableOpacity>
     <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+    <View style={styles.infoContainer}>
+      <Text style={styles.jobName(selectedJob, item)}>
+        {item.job_title}
+      </Text>
+    </View>
    </TouchableOpacity>
   )
 }
