fb702157d9fb72753e6098544df53c7991ef9184
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-02 08:58:18 +0200

    add imports to job details

--- app/job_details/[id].js
+++ app/job_details/[id].js
@@ -1,4 +1,10 @@
-import { Text } from "react-native"
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native"
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hool/useFetch'; 
 
 const JobDetails = () => {
   return (
