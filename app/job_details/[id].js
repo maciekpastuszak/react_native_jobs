5308a98564f560c46afc9a9d031e057766517772
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-03 11:27:59 +0200

    add jobtabs props

--- app/job_details/[id].js
+++ app/job_details/[id].js
@@ -6,6 +6,8 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from '../../hook/useFetch'; 
 
+const tabs = ["About", "Qualifications", "Responsibilities"]
+
 const JobDetails = () => {
     const params = useSearchParams();
     const router = useRouter();
@@ -15,6 +17,7 @@ const JobDetails = () => {
     })
 
     const [refreshing, setRefreshing] = useState(false);
+    const [activeTab, setactiveTab] = useState(tabs[0]);
 
     const onRefresh = () => {}
 
@@ -62,7 +65,9 @@ const JobDetails = () => {
               />
 
               <JobTabs
-    
+                 tabs={tabs}
+                 activeTab={activeTab}
+                 setActiveTab={setActiveTab}
               />
 
               {displayTabContent()}
