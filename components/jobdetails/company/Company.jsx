da5ea8508adf05c96c3948027ae774b0a78c2812
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-03 11:16:55 +0200

    add image to job details

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -9,8 +9,17 @@ import styles from './company.style'
 
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+         <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
     </View>
   )
 }
