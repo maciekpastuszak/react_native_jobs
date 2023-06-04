2655e45e9f6ad2be240cdceb217feeadb7b60618
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-04 21:44:14 +0200

    fix styles

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -9,7 +9,7 @@ const About = ({ info }) => {
       <Text style={styles.headText}>About the job:</Text>
 
       <View style={styles.contentBox}>
-          <Text style={contextText}>{info}</Text>
+          <Text style={styles.contextText}>{info}</Text>
       </View>
     </View>
   )
