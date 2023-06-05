c5cba1f1a5c330e75e848c33cab7e8ea88fbfe4d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-05 21:45:28 +0200

    add hook and props to Welcome component

--- app/index.js
+++ app/index.js
@@ -7,6 +7,8 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 
 const Home = () => {
     const router = useRouter()
+    const [searchTerm, setSearchTerm] = useState("")
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen 
@@ -30,7 +32,13 @@ const Home = () => {
                         padding: SIZES.medium
                     }}
                 >
-                    <Welcome />
+                    <Welcome 
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            router.push(`/search/${searchTerm}`)
+                        }}
+                    />
 
                     <Popularjobs />
                     <Nearbyjobs />
