6f7446e99e1f6db56dfc666ad976cbe92129d62a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-06 14:26:56 +0200

    add handleCardPress

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,8 +13,14 @@ const Popularjobs = () => {
     'search', {
       query: 'React developer',
       num_pages: 1
+    });
+
+    const [selectedJob, setSelectedJob] = useState()
+
+    const handleCardPress = (item) => {
+      router.puch(`/job-details/${item.job_id}`);
+      setSelectedJob(item.job_id)
     }
-  );
 
   return (
     <View style={styles.container}>
